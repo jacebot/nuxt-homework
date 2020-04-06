7a7c580d976eafc72cae6c8b417f0c04e9990bdc
Jace Whitten <jace.whitten@gmail.com>
2020-04-06 13:07:21 -0700

    Update main page

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,17 @@
   <div>
     <div class="container">
       <div>
+        <div @click="setFilterByTag('all')">
+          All
+        </div>
+        <TagButton
+          v-for="tag in tags"
+          :key="tag.$index"
+          :name="tag"
+          @click.native="setFilterByTag(tag)"
+        />
+      </div>
+      <div>
         <logo />
         <h1 class="title">
           qa-app
@@ -10,9 +21,14 @@
           My praiseworthy Nuxt.js project
         </h2>
         <div>
-          <ul v-if="posts.length">
-            <PostLink v-for="post in posts" :key="post.id" :post="post" />
+          <ul v-if="!loading.busy" class="postWrap">
+            <PostLink
+              v-for="post in filterPostsByTag"
+              :key="post.$index"
+              :post="post"
+            />
           </ul>
+          <div v-else><h2>Loading...</h2></div>
         </div>
       </div>
     </div>
@@ -20,38 +36,72 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
 import Logo from '~/components/Logo.vue'
 import PostLink from '~/components/PostLink.vue'
+import TagButton from '~/components/TagButton.vue'
 
 export default {
   components: {
     Logo,
-    PostLink
+    PostLink,
+    TagButton
   },
+
   data() {
     return {
-      posts: []
+      posts: [],
+      tags: [],
+      filterTags: [],
+      tagFilterKey: 'all'
     }
   },
 
-  mounted() {
+  computed: {
+    ...mapState(['loading']),
+    filterPostsByTag(tag) {
+      const newArr = [...this.posts]
+      if (this.tagFilterKey === 'all') {
+        console.log(newArr)
+        return this.posts
+      } else {
+        // const arr = this.tags.filter((t, i) => t === tag)
+        // this.posts.filter((e) => e.tags.includes(this.tagFilterKey))
+        return newArr.filter((e) => e.tags.includes(this.tagFilterKey))
+      }
+    }
+  },
+
+  created() {
     this.getPosts()
   },
 
   methods: {
+    ...mapActions({
+      busyState: 'loading/triggerBusyState',
+      stopBusyState: 'loading/stopBusyState'
+    }),
     async getPosts() {
+      this.busyState()
       try {
         await this.$axios
           .post('list/posts')
           .then((res) => {
             this.posts = [...res.data.list]
+            this.tags = [...res.data.tags]
+            this.stopBusyState()
           })
           .catch((err) => {
             console.log(err)
+            this.stopBusyState()
           })
       } catch (e) {
         this.error = e.response.data.message
+        this.stopBusyState()
       }
+    },
+    setFilterByTag(tag) {
+      this.tagFilterKey = tag
     }
   }
 }
@@ -87,5 +137,14 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.postWrap {
+  list-style-type: none;
+  padding: 2%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  flex-direction: row;
 }
 </style>
