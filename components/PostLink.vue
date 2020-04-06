6948ac2af1d43bb90bad5b4478509afad96aaa92
Jace Whitten <jace.whitten@gmail.com>
2020-04-06 13:06:26 -0700

    Update post styles

--- components/PostLink.vue
+++ components/PostLink.vue
@@ -1,15 +1,15 @@
 <template>
-  <li>
+  <li class="post">
     <nuxt-link
       :to="{
         path: `answer/${post.id}/${post.slug}`,
         params: { id: post.id }
       }"
-      ><h3>
+      ><h3 class="title">
         {{ post.title }}<br />
         <small>{{ post.created_at }}</small>
       </h3>
-      <p>{{ post.body }}</p>
+      <p class="postBody">{{ post.body }}</p>
     </nuxt-link>
   </li>
 </template>
@@ -21,4 +21,22 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.post {
+  padding: 10px;
+  margin: 1% 0;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  width: 46%;
+  text-align: left;
+}
+h3.title {
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin: 0;
+}
+.postBody {
+  display: block;
+  font-size: 0.9rem;
+}
+</style>
